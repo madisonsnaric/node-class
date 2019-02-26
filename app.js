99b6a6edd36a51985db7fa251cc4c662880d7d02
Madison Snaric <snaric@usc.edu>
2019-02-25 20:25:03 -0800

    listen on port process.env.PORT when on Heroku

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 let express = require('express'); 
+let knex = require('knex'); 
 
 let app = express();
 
 app.get('/api/genres', function(request, response) {
-	response.json([1, 2, 3]); 
+	let connection = knex({
+		client: 'sqlite3',
+		connection: {
+			filename: 'chinook.db'
+		}
+	})
+
+	connection.select().from('genres').then((genres) => {
+		response.json(genres); 
+	}); 
 });
 
-app.listen(8000); 
+app.get('/api/genres/:id', function(request, response) {
+	let id = request.params.id; 
+
+
+	let connection = knex({
+		client: 'sqlite3',
+		connection: {
+			filename: 'chinook.db'
+		}
+	}); 
+
+	connection
+		.select()
+		.from('genres')
+		.where('GenreId', id)
+		.first()
+		.then((genre) => {
+			if(genre) {
+				response.json(genre);
+			} else {
+				response.status(404).json({
+					error: `Genre ${id} not found`
+				});
+			}
+	}); 
+});
+
+app.listen(process.env.PORT || 8000); 
