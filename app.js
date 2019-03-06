daa8810571a024777327a220b12ea91c30dd0abc
Madison Snaric <snaric@usc.edu>
2019-03-05 19:46:19 -0800

    node assignment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require('express'); 
 let knex = require('knex'); 
+const querystring = require('querystring'); 
 
 let app = express();
 
@@ -42,5 +43,65 @@ app.get('/api/genres/:id', function(request, response) {
 			}
 	}); 
 });
+
+app.get('/api/artists', function(request, response) {
+	let connection = knex({
+		client: 'sqlite3',
+		connection: {
+			filename: 'chinook.db'
+		}
+	})
+
+	// connection.select().from('artists').then((artists) => {
+	// 	response.json(artists); 
+	// }); 
+
+	let filter = req.query.filter; 
+
+	if (!filter) {
+		connection
+			.select()
+			.from('artists')
+			.then((artists) => {
+			response.json(artists);
+		});
+	}
+	else {
+		connection
+			.select()
+			.from('artists')
+			.where('Name', 'like', filter)
+			.then((artists) => {
+				response.json(artists);
+			}); 
+	}
+
+}); 
+
+// get /api/artists?filter=a
+//reg.query.filter
+
+// app.get('api/artists?filter=a'), function(request, response) {
+// 	//let id = request.params.id; 
+
+// 	let filter = reg.query.filter; 
+
+// 	let connection = knex({
+// 		client: 'sqlite3',
+// 		connection: {
+// 			filename: 'chinook.db'
+// 		}
+// 	})
+
+// 	connection 
+// 		.select()
+// 		.from('artists')
+// 		.where('Name', 'like', filter)
+// 		.then((artists) => {
+// 			response.json(artists);
+// 		}); 
+// }; 
+
+
 
 app.listen(process.env.PORT || 8000); 
